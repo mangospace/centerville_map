cf97af8e3ceb0ddffbbaf7551ac42f67de69b23d
Manas Kaushik, MD ScD <kaushik.manas@gmail.com>
2023-12-05 21:39:09 -0500

    first commit

--- map_centerville_locations.py
+++ map_centerville_locations.py
@@ -4,16 +4,15 @@ import numpy as np
 import folium
 import streamlit as st
 from streamlit_folium import st_folium
-full_data=pd.read_csv(r"D:\Data\Twitter\Centerville_location_map\allcenters.csv")
 
-
-full_data=pd.read_csv('https://raw.githubusercontent.com/mangospace/OpTreat/main/oct_data.csv')
+full_data=pd.read_csv('https://raw.githubusercontent.com/mangospace/centerville_map/main/allcenters.csv')
+full_data["STATE"]= full_data["State-zip"].str.split(" ")
 full_data.reset_index(drop=True)
 statelist=full_data['STATE'].tolist()
 statelist.insert(0, "US")
 
-st.title('Opiod Treatment Centers')
-st.caption("Oct 2023")
+st.title('Humana Centerville Centers')
+st.caption("Dec 2023")
 st.caption('Made by @manas8u')
 st.caption('Please share your feedback and suggestions. DM @manas8u')
 
@@ -24,12 +23,13 @@ option1 = st.selectbox(
 state_data=pd.read_csv('https://raw.githubusercontent.com/mangospace/OpTreat/main/state_center.csv', dtype={'lat':np.int32,	'lon':np.int32})
 df2 = pd.DataFrame({"lat":[48],
                     "lon":[-102],
-                    "STATE":["US"]})
+                    "State or federal district":["US"]})
 
 state_data=pd.concat([state_data,df2])
-state_data=state_data[state_data["STATE"]==option1]
+state_data=state_data[state_data["State or federal district"]==option1]
 state_data=state_data.reset_index(drop=True)
 long=state_data.loc[0,'lon']
+
 latt=state_data.loc[0,'lat']
 
 if option1=="US":
@@ -43,9 +43,9 @@ else:
 m = folium.Map(location=[latt, long], zoom_start=zoom_start_var)
 for x in range(len(full_data1)):
     folium.Marker(
-        location=[full_data1.loc[x,'lat'],full_data1.loc[x,'lon']],
+        location=[full_data1.loc[x,'latitude'],full_data1.loc[x,'longitude']],
         tooltip=full_data1.loc[x,'CITY'],
-        popup=full_data1.loc[x,'PROVIDER NAME'],
+        popup=full_data1.loc[x,'Center_Name'],
         icon=folium.Icon(color="green"),
     ).add_to(m)
 st_data = st_folium(m, width=725)
